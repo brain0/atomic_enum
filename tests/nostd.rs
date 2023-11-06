3c901189fa4b7abeb500483468f9640e811fd161
Ryan Butler <thebutlah@gmail.com>
2023-11-05 22:39:59 -0500

    Create CAS feature for better no_std support

--- tests/nostd.rs
+++ tests/nostd.rs
@@ -14,12 +14,36 @@ enum FooBar {
     Bar,
 }
 
+#[cfg(feature = "cas")]
 #[test]
 fn test_no_std_use() {
     let fb = AtomicFooBar::new(FooBar::Foo);
-    let prev = fb.compare_exchange(FooBar::Foo, FooBar::Bar, Ordering::SeqCst, Ordering::Relaxed).unwrap();
+    let prev = fb
+        .compare_exchange(
+            FooBar::Foo,
+            FooBar::Bar,
+            Ordering::SeqCst,
+            Ordering::Relaxed,
+        )
+        .unwrap();
     assert_eq!(prev, FooBar::Foo);
 
-    let prev_fail = fb.compare_exchange(FooBar::Foo, FooBar::Bar, Ordering::SeqCst, Ordering::Relaxed);
+    let prev_fail = fb.compare_exchange(
+        FooBar::Foo,
+        FooBar::Bar,
+        Ordering::SeqCst,
+        Ordering::Relaxed,
+    );
     assert!(prev_fail.is_err());
+}
+
+#[test]
+fn test_load_store() {
+    let original = FooBar::Foo;
+    let fb = AtomicFooBar::new(original);
+    assert_eq!(fb.load(Ordering::SeqCst), original);
+
+    let new = FooBar::Bar;
+    fb.store(new, Ordering::SeqCst);
+    assert_eq!(fb.load(Ordering::SeqCst), new);
 }
